8dff18e45db3011be873a384245857b51c0ca7c3
Mohammad Zeynali <mhmdzynaly977@gmail.com>
2023-05-10 01:44:34 -0700

    update project

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
       colors: {
         primary: "#0ea960",
         secondary: "#2a2a2a",
-        neutral: "#neutral",
+        neutral: "#6b6b6b",
         shadow: "rgba(0,0,0,0.1)",
       },
 
